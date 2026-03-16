d7d73d062adb5f2adfb8adfb8db2b76d6f39f518
Renan <renanlopesteles@gmail.com>
2026-03-16 19:01:02 -0300

    Reconfigurações de CORS

--- src/main/java/com/strongmemoryapi/security/SecuritySecurity.java
+++ src/main/java/com/strongmemoryapi/security/SecuritySecurity.java
@@ -4,6 +4,7 @@ import com.strongmemoryapi.security.jwt.JwtAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -14,7 +15,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
-public class AuthSecurity {
+public class SecuritySecurity {
 
     @Autowired
     private JwtAuthenticationFilter filter;
@@ -32,6 +33,8 @@ public class AuthSecurity {
                 .authorizeHttpRequests(auth -> auth
 
                         // PermitAll
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+
                         .requestMatchers(
                                 "/api/player/auth",
                                 "/api/player/register",
